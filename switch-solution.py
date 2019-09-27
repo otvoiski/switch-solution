9173c38ac862a13726d355ff1aeaab0984963e4e
Otavio Voiski <otavio.voiski@energisa.com.br>
2019-09-27 14:01:40 -0300

    ajuste

--- switch-solution.py
+++ switch-solution.py
@@ -5,7 +5,7 @@ from service.commandService import *
 from model.ambiente import Ambiente
 
 
-def loading(argv):
+def loading():
     # exemplo -a ho -t ws -n WSXYZ
     parser = argparse.ArgumentParser(description="Switch Solution")
     parser.add_argument("name", type=str, help='Nome do projeto: ex=wsxyz')
@@ -13,20 +13,19 @@ def loading(argv):
                         help='Ambiente de Desenvolvimento.', default=False)
     parser.add_argument("-ho", "--hom", action="store_true",
                         help='Ambiente de Homologação.',  default=False)
-    parser.add_argument("-i", "--info", action="store_true",
-                        help='Informações do sistema',  default=False)
     parser.add_argument("-c", "--config", action="store_true",
                         help='Arquivo de configuração do sistema', default=False)
     return parser.parse_args()
 
 
 def main(argv: None):
-    arguments = loading(argv)
-    ambiente = Ambiente(arguments)
+    arguments = loading()
 
     if arguments.config == True:
         os.system(".\\switch-solution\\config\\config.json")
         return
+
+    ambiente = Ambiente(arguments)
 
     if ambiente.enviroment != None:
         # direct command this is optional, use to be fast!
