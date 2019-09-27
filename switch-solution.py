320b64ba8d3e3d9295c6c8a5ec242039c95f30c7
Otavio Voiski <otavio.voiski@energisa.com.br>
2019-09-27 10:36:49 -0300

    ajustes

--- switch-solution.py
+++ switch-solution.py
@@ -15,12 +15,18 @@ def loading(argv):
                         help='Ambiente de Homologação.',  default=False)
     parser.add_argument("-i", "--info", action="store_true",
                         help='Informações do sistema',  default=False)
+    parser.add_argument("-c", "--config", action="store_true",
+                        help='Arquivo de configuração do sistema', default=False)
     return parser.parse_args()
 
 
 def main(argv: None):
     arguments = loading(argv)
     ambiente = Ambiente(arguments)
+
+    if arguments.config == True:
+        os.system(".\\switch-solution\\config\\config.json")
+        return
 
     if ambiente.enviroment != None:
         # direct command this is optional, use to be fast!
